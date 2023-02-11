fd8aefb8e603c3212cb2208251685c46d2ab0e36
Stefan Breetveld <spiderbiggen@gmail.com>
2023-02-11 18:19:37 +0100

    clean up imports

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 use std::net::SocketAddr;
 
 use anyhow::Result;
-use axum::extract::{Query, State};
-use axum::response::{IntoResponse, Response};
-use axum::{extract::Path, http::StatusCode, routing::get, Json, Router};
-use k8s_openapi::api::apps::v1::Deployment;
-use kube::api::Api;
-use kube::api::{Patch, PatchParams, ValidationDirective};
-use kube::error::Error as KubeError;
+use axum::response::IntoResponse;
+use axum::{routing::get, Router};
 use serde::Deserialize;
-use serde_json::{json, Value};
 use tower::ServiceBuilder;
 use tower_http::compression::CompressionLayer;
 use tower_http::decompression::DecompressionLayer;
 use tower_http::trace::TraceLayer;
-use tracing::instrument;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-use crate::auth::{AuthState, User};
 use crate::models::AppState;
 
 mod auth;
